9c41acc9dee4c6ef78da23025d8e310feafdf4a9
Ben Evans <jedifreeman@gmail.com>
2014-01-22 16:03:37 -0800

    Fix description that shows up in Radiant Admin

--- lib/radiant-radiant_validators-extension.rb
+++ lib/radiant-radiant_validators-extension.rb
@@ -1,7 +1,7 @@
 module RadiantRadiantValidatorsExtension
   VERSION     = "1.1.1"
   SUMMARY     = "Radiant Validators for Radiant CMS"
-  DESCRIPTION = "Makes Radiant better by adding radiant_validators!"
+  DESCRIPTION = "Extension to allow overriding of built-in validators in Radiant"
   URL         = "http://github.com/JediFreeman/Radiant-Validators-Extension"
   AUTHORS     = ["Ben Evans"]
   EMAIL       = ["jedifreeman@gmail.com"]
