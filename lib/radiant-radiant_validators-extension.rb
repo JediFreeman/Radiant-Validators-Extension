0d14cee58abbc0ed11b95fdd1e75d9e9b943d03a
Ben Evans <jedifreeman@gmail.com>
2014-01-22 16:18:13 -0800

    bump version, skipping to 1.1.4 to sync with radiant

--- lib/radiant-radiant_validators-extension.rb
+++ lib/radiant-radiant_validators-extension.rb
@@ -1,5 +1,5 @@
 module RadiantRadiantValidatorsExtension
-  VERSION     = "1.1.1"
+  VERSION     = "1.1.4"
   SUMMARY     = "Radiant Validators for Radiant CMS"
   DESCRIPTION = "Extension to allow overriding of built-in validators in Radiant"
   URL         = "http://github.com/JediFreeman/Radiant-Validators-Extension"
